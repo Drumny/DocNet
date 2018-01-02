0d272ec85120ed89cd5d72848c50a26f1258c14c
Drumny <34957179+Drumny@users.noreply.github.com>
2018-01-02 10:41:49 +0800

    Add files via upload

---
+++ Demo/SmsSent/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SmsSent
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+            Client client =Client.getInstance();
+		}
+	}
+}

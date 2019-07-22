87dfa8078c5f3df755c1631a4292b883ca09855b
johnnyhe <johnnyhe@aslcn.com.cn>
2019-07-22 18:24:51 +0800

    init

---
+++ Src/Src.Common/Tools.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Src.Common
+{
+    public class Tools
+    {
+        /// <summary>
+        /// 获取并创建根目录外的兄弟目录
+        /// </summary>
+        /// <param name="folderName"></param>
+        /// <returns></returns>
+        public static string GetSiblingFolderPath(string folderName)
+        {
+            var sibing = "";
+            var index = AppContext.BaseDirectory.ToLower().IndexOf(@"\bin\debug\");
+            if (index > -1)
+            {
+                sibing = AppContext.BaseDirectory.Substring(0, index);
+            }
+            var directoryInfo = System.IO.Directory.GetParent(sibing);
+            sibing = System.IO.Path.Combine(directoryInfo.FullName, folderName);
+
+            if (!System.IO.Directory.Exists(sibing))
+            {
+                System.IO.Directory.CreateDirectory(sibing);
+            }
+            return sibing;
+        }
+    }
+}
---
+++ Src/Src.ConsoleApp/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Src.ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var text = Common.Tools.GetSiblingFolderPath("Log");
+            Console.WriteLine(text);
+            Console.ReadKey();
+        }
+    }
+}
